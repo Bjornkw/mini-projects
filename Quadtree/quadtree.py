1a286f5e13214660cbb56da66091dcec0ef5af88
Björn Krook Willén <bjorn-.krook.willen@gmail.com>
2022-11-03 22:03:31 +0100

    updated quadtree

--- Quadtree/quadtree.py
+++ Quadtree/quadtree.py
@@ -5,6 +5,18 @@ Created on Thu Nov  3 00:02:14 2022
 @author: Björn
 """
 
+###############################################################################
+
+class point():
+    def __init__(self, x, y, idx=None):
+        self.x = x
+        self.y = y
+        self.idx = idx
+        
+    def __str__(self):
+        return ("Point " + str(self.idx) + ": x = " + str(self.x) + ", y = " + str(self.y))
+        
+###############################################################################
 
 class QuadTree():
     def __init__(self, layer=0, bounds=[0,0,1,1]):
@@ -12,39 +24,35 @@ class QuadTree():
         self.points =[]
         self.maxpoints = 5
         self.bounds = bounds
+        self.midpoint = [(bounds[2]-bounds[0])/2, (bounds[3]-bounds[1])/2]
         self.children = False
+        
+    def __str__(self):
+        return "Quadtree at layer " + str(self.layer)
     
-    def add_point(self, point):
-        self.points.append(point)
-        if (len(self.points) > self.maxpoints) or self.children:
-            self.children = True
-            midpoint = [(self.bounds[0]-self.bounds[2])/2, (self.bounds[1]-self.bounds[3])/2]
-            self.q1 = QuadTree(self.layer+1, [midpoint[0], self.bounds[2], midpoint[1], self.bounds[3]])
-            self.q2 = QuadTree(self.layer+1, [self.bounds[0], midpoint[0], midpoint[1], self.bounds[3]])
-            self.q3 = QuadTree(self.layer+1, [self.bounds[0], midpoint[0], self.bounds[1], midpoint[1]])
-            self.q4 = QuadTree(self.layer+1, [midpoint[0], self.bounds[2], self.bounds[2], midpoint[1]])
-            for point in self.points:
-                if point[0] > midpoint[0]:
-                    if point[1] > midpoint[1]:
-                        self.q1.add_point(point)
-                    else:
-                        self.q4.add_point(point)
-                else:
-                    if point[1] > midpoint[1]:
-                        self.q2.add_point(point)
-                    else:
-                        self.q3.add_point(point)
-            self.points = []
-                    
+    def add_single_point(self, point):
+        if self.children:
+            self.add_to_children(point)
         else:
             self.points.append(point)
+            
+    def add_points(self, points):
+        for point in points:
+            self.add_single_point(point)
+            
+    def add_to_children(self, point):
+        pass
+    
+###############################################################################
     
 if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt
     import time
     
-    points = [np.random.random([2]) for i in range(20)]
+    points = [point(np.random.random(), np.random.random(), i) for i in range(10000)]
     tree = QuadTree()
-    for point in points:
-        tree.add_point(point)
+    start = time.time()
+    tree.add_points(points)
+    end = time.time()
+    print(end-start)
