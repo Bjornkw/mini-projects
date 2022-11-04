abee1331b1c01e1a7ce7ae772f1a52f55fc2d9af
Björn Krook Willén <bjorn-.krook.willen@gmail.com>
2022-11-04 22:11:27 +0100

    finished ant colony

--- Ant colony optimization/antcolony.py
+++ Ant colony optimization/antcolony.py
@@ -6,7 +6,6 @@ Created on Fri Oct 28 01:08:08 2022
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 def LenPath(points, path):
     dist = 0
@@ -14,59 +13,95 @@ def LenPath(points, path):
         dx = points[0, path[i+1]] - points[0, path[i]]
         dy = points[1, path[i+1]] - points[1, path[i]]
         dist = dist + np.linalg.norm([dx, dy])
-        
     return dist
+        
+def choose_from(array):
+    norm_array = array/sum(array)
+    r = np.random.random()
+    s = 0
+    for i in range(len(array)):
+        s = s + norm_array[i]
+        if s>r:
+            return i
+    raise Exception('cum')
 
-def AntWalk(points, ph_level, ds_level):
-    not_visited = list(range(len(points[0])))
+def AntWalk(points, ph_level, ds_level, alpha, beta):
+    not_visited = np.array(range(len(points[0])), dtype=int)
     path = []
     path.append(np.random.permutation(not_visited)[0])
-    not_visited[path[0]] = []
+    np.delete(not_visited, path[0])
     while len(not_visited) > 0:
         ph = ph_level[not_visited, path[-1]]
         ds = ds_level[not_visited, path[-1]]
-        prob = [i for i in range(len(not_visited))]
-        
-        
-    return path
-    
-def PhUpdate(ph_level, paths):
-    pass
+        p = (ph**alpha)*(ds**beta)
+        idx = choose_from(p)
+        choise = not_visited[idx]
+        path.append(choise)
+        not_visited = np.delete(not_visited, idx)
+    path.append(path[0])
+    return np.array(path)
 
-def AntOptim(points, max_iter, alpha, beta, decay):
     
-    best_path = np.zeros(len(points[0])+1)
+def PhUpdate(points, ph_level, paths, decay, ph_min, Q):
+    dph = ph_level*0
+    for path in paths:
+        l = LenPath(points, path)
+        for i in range(len(path)-1):
+            dph[path[i], path[i+1]] = Q/l
+            dph[path[i+1], path[i]] = Q/l
+    ph_level = (ph_level+dph)*(1-decay)
+    for i in range(len(ph_level[0])):
+        for j in range(len(ph_level[0])):
+            if ph_level[i,j]<ph_min:
+                ph_level[i, j] = ph_min
+    return ph_level 
+
+def AntOptim(points, max_iter=100, alpha=1, beta=2, decay=0.01, ph_min=0.01, Q=1):
+    best_path = np.array(range(len(points[0])))
     path_lengths = []
     ph_level = np.ones([len(points[0]), len(points[0])])
     ds_level = np.ones([len(points[0]), len(points[0])])
-    
     for i in range(len(points[0])):
         for j in range(len(points[0])):
             if i == j:
                 ds_level[i, j] = 0
             else:
                 ds_level[i, j] = 1/LenPath(points, [i, j])
-    
     for i in range(max_iter):
+        print("iteration " + str(i) + "/" + str(max_iter))
         paths = []
         for j in range(n_ants):
-            path = AntWalk(points, ph_level, ds_level)
+            path = AntWalk(points, ph_level, ds_level, alpha, beta)
             paths.append(path)
-            
-        ph_level = PhUpdate(ph_level, paths)
+            if LenPath(points, path) < LenPath(points, best_path):
+                best_path = path
+        ph_level = PhUpdate(points, ph_level, paths, decay, ph_min, Q)
+        path_lengths.append(LenPath(points, best_path))
+    return best_path, path_lengths, ph_level
+
+def PlotStuff(points, best_path, lens, ph_level):
+    plt.figure()
+    fig = plt.scatter(points[0], points[1])
+    for i in range(len(best_path)-1):
+        pass
+        plt.plot((points[0][best_path[i]], points[0][best_path[i+1]]), (points[1][best_path[i]], points[1][best_path[i+1]]), 'r')
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
+    plt.figure()
+    fig2 = plt.plot(range(len(lens)), lens)
     
-    return ds_level, path_lengths
 
+if __name__=="__main__":
+    import matplotlib.pyplot as plt
+    n_points = 50
+    n_ants = 100
+    points = np.random.random([2, n_points])
+    max_iter = 2000
+    alpha = 1.5
+    beta = 2.5
+    decay = 0.02
+    ph_min = 0.05
+    Q = 1
 
-n_points = 25
-n_ants = 25
-points = np.random.random([2, n_points])
-max_iter = 100
-alpha = 1
-beta = 2
-decay = 0.02
-
-best, lens = AntOptim(points, max_iter, alpha, beta, decay)
-fig = plt.scatter(points[0], points[1])
-fig.axes.get_xaxis().set_visible(False)
-fig.axes.get_yaxis().set_visible(False)
+    best, lens, ph = AntOptim(points, max_iter, alpha, beta, decay, ph_min, Q)
+    PlotStuff(points, best, lens, ph)
--- Quadtree/quadtree.py
+++ Quadtree/quadtree.py
@@ -25,23 +25,59 @@ class QuadTree():
         self.maxpoints = 5
         self.bounds = bounds
         self.midpoint = [(bounds[2]-bounds[0])/2, (bounds[3]-bounds[1])/2]
-        self.children = False
+        self.has_children = False
+        self.q1 = None
+        self.q2 = None
+        self.q3 = None
+        self.q4 = None
         
     def __str__(self):
         return "Quadtree at layer " + str(self.layer)
     
     def add_single_point(self, point):
-        if self.children:
+        if self.has_children:
             self.add_to_children(point)
         else:
-            self.points.append(point)
+            if len(self.points) < self.maxpoints+1:
+                self.points.append(point)
+            else:
+                self.has_children = True
+                self.make_children()
+                self.add_to_children(point)
+                for p in self.points:
+                    self.add_to_children(p)
+                self.points = []
             
     def add_points(self, points):
         for point in points:
             self.add_single_point(point)
             
+    def make_children(self):
+        x_min = self.bounds[0]
+        y_min = self.bounds[1]
+        x_max = self.bounds[2]
+        y_max = self.bounds[3]
+        self.q1 = QuadTree(self.layer+1, [self.midpoint[0], self.midpoint[1], x_max, y_max])
+        self.q2 = QuadTree(self.layer+1, [x_min, self.midpoint[1], self.midpoint[0], y_max])
+        self.q3 = QuadTree(self.layer+1, [self.midpoint[0], y_min, x_max, self.midpoint[1]])
+        self.q4 = QuadTree(self.layer+1, [x_min, y_min, self.midpoint[0], self.midpoint[1]])
+        
+            
     def add_to_children(self, point):
+        if point.x >= self.midpoint[0]:
+            if point.y >= self.midpoint[1]:
+                self.q1.add_single_point(point)
+            else:
+                self.q4.add_single_point(point)
+        else:
+            if point.y >= self.midpoint[1]:
+                self.q2.add_single_point(point)
+            else:
+                self.q3.add_single_point(point)
+                
+    def query(self, point, radius):
         pass
+    
     
 ###############################################################################
     
@@ -50,7 +86,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import time
     
-    points = [point(np.random.random(), np.random.random(), i) for i in range(10000)]
+    points = [point((np.random.random()+0.01)/1.02, np.random.random(), i) for i in range(20)]
     tree = QuadTree()
     start = time.time()
     tree.add_points(points)
