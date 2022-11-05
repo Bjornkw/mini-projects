388c613a16f6ad0840fe95368a2d7f57ceccd5e2
Björn Krook Willén <bjorn.krook.willen@gmail.com>
2022-11-05 01:34:18 +0100

    added boids

--- Quadtree/quadtree.py
+++ Quadtree/quadtree.py
@@ -19,65 +19,45 @@ class point():
 ###############################################################################
 
 class QuadTree():
-    def __init__(self, layer=0, bounds=[0,0,1,1]):
-        self.layer = layer
-        self.points =[]
-        self.maxpoints = 5
+    def __init__(self, depth=0, bounds=[0,0,1,1]):
+        self.layer = depth
+        self.maxdepth = 5
         self.bounds = bounds
         self.midpoint = [(bounds[2]-bounds[0])/2, (bounds[3]-bounds[1])/2]
-        self.has_children = False
+        self.children = []
+        self.points = []
+        self.maxpoints = 10
         self.q1 = None
         self.q2 = None
         self.q3 = None
         self.q4 = None
+
         
-    def __str__(self):
-        return "Quadtree at layer " + str(self.layer)
-    
-    def add_single_point(self, point):
-        if self.has_children:
-            self.add_to_children(point)
-        else:
-            if len(self.points) < self.maxpoints+1:
-                self.points.append(point)
-            else:
-                self.has_children = True
-                self.make_children()
-                self.add_to_children(point)
-                for p in self.points:
-                    self.add_to_children(p)
-                self.points = []
-            
-    def add_points(self, points):
-        for point in points:
-            self.add_single_point(point)
-            
-    def make_children(self):
-        x_min = self.bounds[0]
-        y_min = self.bounds[1]
-        x_max = self.bounds[2]
-        y_max = self.bounds[3]
-        self.q1 = QuadTree(self.layer+1, [self.midpoint[0], self.midpoint[1], x_max, y_max])
-        self.q2 = QuadTree(self.layer+1, [x_min, self.midpoint[1], self.midpoint[0], y_max])
-        self.q3 = QuadTree(self.layer+1, [self.midpoint[0], y_min, x_max, self.midpoint[1]])
-        self.q4 = QuadTree(self.layer+1, [x_min, y_min, self.midpoint[0], self.midpoint[1]])
-        
-            
-    def add_to_children(self, point):
-        if point.x >= self.midpoint[0]:
-            if point.y >= self.midpoint[1]:
-                self.q1.add_single_point(point)
-            else:
-                self.q4.add_single_point(point)
-        else:
-            if point.y >= self.midpoint[1]:
-                self.q2.add_single_point(point)
-            else:
-                self.q3.add_single_point(point)
-                
-    def query(self, point, radius):
+    def insert(self, points):
         pass
     
+    def subdivide(self):
+        
+        boundsq1 = 0
+        boundsq2 = 0
+        boundsq3 = 0
+        boundsq4 = 0
+        
+        self.q1 = QuadTree(self.depth+1, boundsq1)
+        self.q2 = QuadTree(self.depth+1, boundsq2)
+        self.q3 = QuadTree(self.depth+1, boundsq3)
+        self.q4 = QuadTree(self.depth+1, boundsq4)
+        
+    def is_inside(self, point):
+        if (self.bounds[0] < point.x < self.bounds[2]) and (self.bounds[1] < point.y < self.bounds[3]):
+            return True
+        else:
+            return False
+    
+    def query(self, vec, r):
+        pass
+        
+
     
 ###############################################################################
     
@@ -89,6 +69,6 @@ if __name__ == "__main__":
     points = [point((np.random.random()+0.01)/1.02, np.random.random(), i) for i in range(20)]
     tree = QuadTree()
     start = time.time()
-    tree.add_points(points)
+    tree.insert(points)
     end = time.time()
     print(end-start)
